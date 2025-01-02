b41615f4ac8239b5fc11357f41cfa8f5b5276656
Aditya <137521434+adityarise2@users.noreply.github.com>
2025-01-02 22:49:38 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -54,7 +54,7 @@ async def send_movie_updates(bot, file_name, file_id):
     if not poster_url or not caption:
         return
     btn = [
-        [InlineKeyboardButton('Get File', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')]
+        [InlineKeyboardButton('📂 Get Files Bot', url=f'https://telegram.me/AllMoviesRobot?start=getfile-{movie_name.replace(" ", "-")}')]
     ]
     reply_markup = InlineKeyboardMarkup(btn)
     movie_update_channel =await db.movies_update_channel_id()
